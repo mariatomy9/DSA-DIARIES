10c6503e5efe4ae162b5c0f3f71d147a7af723ce
AmalChandru <amaltathva@gmail.com>
2021-01-01 21:10:25 +0530

    updated code files

---
+++ Codechef/IARCSJUD/BOOKLIST.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int main(){
+  int m;
+  cin >> m ;
+  int A[m];
+  for(int i = 0; i < m; i++){
+	  cin >> A[i];
+	  }
+  int n;
+  cin >> n;
+  for(int i = 0; i<m; i++){
+	  cin >> 
+	  }
+
+
+}
