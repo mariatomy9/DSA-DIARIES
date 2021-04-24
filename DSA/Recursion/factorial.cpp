13b2e3e82842158d571fc4f00eb4ce1a414838f8
AmalChandru <amaltathva@gmail.com>
2021-04-24 12:29:56 +0530

    base cases of recursion

---
+++ DSA/Recursion/factorial.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+if(n == 0 || n == 1)
+{
+
+    return 1;
+}
+
+return n*(n-1);
+
+}
+
+int main()
+{
+
+int n;
+cin >> n ;
+cout << fact(n);
+
+
+}
