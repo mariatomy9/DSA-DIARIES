24e3bfb06d62b31848db9d0a5386987f490849a6
AmalChandru <amaltathva@gmail.com>
2021-05-12 14:56:43 +0530

    largest element in an array

---
+++ DSA/Array/largestElement.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler
+
+//Macros for loops
+#define for0(i, n) for (int i = 0; i < (int)(n); i++)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+
+using namespace std;
+
+int LargestElement(int arr[], int x){
+
+//some variables
+int n = sizeof(arr[])/sizeof(arr[0]);
+int largestElement = 0;
+
+forr0(i,n){
+    if(arr[i]>largestElement){
+        largestElement = arr[i];
+    }
+
+
+}
+return LargestElement;
+}
+
+int indexOfLargestElement(){
+int index = LargestElement()
+
+}
+int main(){
+
+
+
+
+
+return 0;
+}
+
