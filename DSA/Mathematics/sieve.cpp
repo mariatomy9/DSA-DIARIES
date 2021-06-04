372078209bedc98e51fdcf06ae1698e1174057f8
AmalChandru <amaltathva@gmail.com>
2021-06-04 13:18:37 +0530

    W1D4

---
+++ DSA/Mathematics/sieve.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler
+
+//Macros for loops
+#define for0(i, n) for (int i = 0; i < (int)(n); i++)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+
+using namespace std;
+ void sieve(int n)
+{
+	if(n <= 1)
+		return;
+
+	bool isPrime[n+1];
+
+	fill(isPrime, isPrime + n + 1, true);
+
+	for(int i=2; i*i <= n; i++)
+	{
+		if(isPrime[i])
+		{
+			for(int j = 2*i; j <= n; j = j+i)
+			{
+				isPrime[j] = false;
+			}
+		}
+	}
+
+	for(int i = 2; i<=n; i++)
+	{
+		if(isPrime[i])
+			cout<<i<<" ";
+	}
+}
+int main(){
+
+
+
+
+
+return 0;
+}
+
