904724a47d1e2e0f5aa271e21de8e4b247c1ea20
AmalChandru <amaltathva@gmail.com>
2021-06-14 17:43:26 +0530

    binarysearch

---
+++ DSA/Searching/indexOfFirstOccurence.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler
+
+//Macros for loops
+#define for0(i, n) for (int i = 0; i < (int)(n); i++)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+
+using namespace std;
+int firstOcc(int arr[], int n, int x)
+{
+	int low = 0, high = n - 1;
+
+	while(low <= high)
+	{
+		int mid = (low + high) / 2;
+
+		if(x > arr[mid])
+			low = mid + 1;
+
+		else if(x < arr[mid])
+			high = mid - 1;
+
+		else
+		{
+			if(mid == 0 || arr[mid - 1] != arr[mid])
+				return mid;
+
+			else
+				high = mid - 1;
+		}
+
+	}
+
+	return -1;
+}
+int main(){
+
+
+
+
+
+return 0;
+}
+
