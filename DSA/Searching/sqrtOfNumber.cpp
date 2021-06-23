00925ccbfe92367f5867a5edd5a50f67bd1ffba0
AmalChandru <amaltathva@gmail.com>
2021-06-23 20:19:44 +0530

    sqrt of a number

---
+++ DSA/Searching/sqrtOfNumber.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler
+
+//Macros for loops
+#define for0(i, n) for (int i = 0; i < (int)(n); i++)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+
+using namespace std;
+
+int sqrtOfNumber(int x)
+{
+   int i = 1; 
+   while(i*i <= x){
+   i++
+   }
+return i - 1;
+}
+
+
+int main(){
+
+
+
+
+
+return 0;
+}
+
