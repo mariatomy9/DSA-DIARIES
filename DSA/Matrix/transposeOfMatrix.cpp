912c0e716973da90a40ce41e5200352d0c3ffca0
AmalChandru <amaltathva@gmail.com>
2021-07-09 20:27:36 +0530

    matrix

---
+++ DSA/Matrix/transposeOfMatrix.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler
+
+//Macros for loops
+#define for0(i, n) for (int i = 0; i < (int)(n); i++)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+
+using namespace std;
+
+int main(){
+ils
+
+
+
+
+return 0;
+}
+
