823f7a0450bdac7ed6814d3e46f6b820106096f1
AmalChandru <amaltathva@gmail.com>
2021-07-14 10:23:39 +0530

    frequencies of elements

---
+++ DSA/Hashing/frequenciesOfArrayElements.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler
+
+//Macros for loops
+#define for0(i, n) for (int i = 0; i < (int)(n); i++)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+
+using namespace std;
+
+void countDistinct(int arr[], int n)
+{
+
+ unordered_map<int, int> mp;
+ 
+    // Traverse through array elements and
+    // count frequencies
+    for (int i = 0; i < n; i++)
+        mp[arr[i]]++;
+ 
+    // Traverse through map and print frequencies
+    for (auto x : mp)
+        cout << x.first << " " << x.second << endl;
+}
+
+int main()
+{
+
+int A[] = {10, 20, 20, 10, 30, 10};
+int n = sizeof(A)/sizeof(A[0]);
+countDistinct(A, n);
+return 0;
+}
+
