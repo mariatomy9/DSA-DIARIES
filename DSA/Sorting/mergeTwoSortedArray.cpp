ba7ca824fc6a57e8aec2a875a32850871948fff8
AmalChandru <amaltathva@gmail.com>
2021-08-03 06:28:45 +0530

    merge two sorted arrays

---
+++ DSA/Sorting/mergeTwoSortedArray.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler
+
+//Macros for loops
+#define for0(i, n) for (int i = 0; i < (int)(n); i++)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+
+using namespace std;
+
+void merge(int a[], int b[], int m, int n)
+{
+    
+}
+int main(){
+    
+    int a[]={10,15,20,40};
+    int b[]={5,6,6,10,15};
+	
+	int m=sizeof(a)/sizeof(a[0]);
+	int n=sizeof(b)/sizeof(b[0]);
+	merge(a,b,m,n);
+
+    return 0;
+}
+
