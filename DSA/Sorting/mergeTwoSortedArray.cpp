d0997deec76d5ed71a3cf115616c8230fa2c1714
AmalChandru <amaltathva@gmail.com>
2021-08-03 06:40:49 +0530

    pointer to pointer

--- DSA/Sorting/mergeTwoSortedArray.cpp
+++ DSA/Sorting/mergeTwoSortedArray.cpp
@@ -8,7 +8,30 @@ using namespace std;
 
 void merge(int a[], int b[], int m, int n)
 {
-    
+    int i, j = 0;
+    while(i < m && i < n)
+    {
+        if(a[i] <=  b[j])
+        {
+            cout << a[i] << " ";
+            i++;
+        }
+        else
+        {
+            cout << b[j] << " ";
+            j++;
+        }
+    }
+    while(i < m)
+    {
+        cout << a[i] << " ";
+        i++;
+    }
+    while(j < n)
+    {
+        cout << b[j] << " ";
+        j++;
+    }
 }
 int main(){
     
