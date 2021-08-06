28061ddbd72aa98201e5c5a10dcd460b14706356
AmalChandru <amaltathva@gmail.com>
2021-08-06 06:19:32 +0530

    intersection of arrays

---
+++ DSA/Searching/intersectionOfArrayEfficient.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler
+
+//Macros for loops
+#define for0(i, n) for (int i = 0; i < (int)(n); i++)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+
+using namespace std;
+void intersection(int a[], int b[], int m, int n)
+{
+    int i, j = 0;
+    while(i < m && i < n)
+    {
+        if(i > 0 && a[i - 1]==a[i])
+        {
+            continue;
+        }
+        if(a[i]<b[j])
+        {
+            i++;
+        }
+        else if(b[j]>a[i])
+        {
+            j++;
+        }
+        else
+        {
+            cout << a[i] << " ";
+            i++;
+            j++;
+        }
+    }
+}
+int main(){
+
+    int a[]={3,5,10,10,10,15,15,20};
+    int b[]={5,10,10,15,30};
+	
+	int m=sizeof(a)/sizeof(a[0]);
+	int n=sizeof(b)/sizeof(b[0]);
+	intersection(a,b,m,n);
+
+
+
+return 0;
+}
+
