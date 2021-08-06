352b08ddd0eb680334d331901b33103191a86637
AmalChandru <amaltathva@gmail.com>
2021-08-06 06:25:34 +0530

    intersection of arrays

--- DSA/Searching/intersectionOfArrayEfficient.cpp
+++ DSA/Searching/intersectionOfArrayEfficient.cpp
@@ -12,13 +12,14 @@ void intersection(int a[], int b[], int m, int n)
     {
         if(i > 0 && a[i - 1]==a[i])
         {
+            i++;
             continue;
         }
         if(a[i]<b[j])
         {
             i++;
         }
-        else if(b[j]>a[i])
+        else if(a[i]>b[j])
         {
             j++;
         }
