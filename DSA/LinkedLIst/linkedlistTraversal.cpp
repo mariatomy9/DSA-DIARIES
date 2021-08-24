ea733a676f09b538525c48f7f0d0d8364c674255
Amal Chandran M V <amal.chandran@clusterdev.net>
2021-08-24 11:11:20 +0530

    traversing through linkedlist

---
+++ DSA/LinkedLIst/linkedlistTraversal.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler
+
+//Macros for loops
+#define for0(i, n) for (int i = 0; i < (int)(n); i++)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+
+using namespace std;
+struct Node{
+
+    int data;
+
+    Node* next;
+    Node(int x)
+    {
+        data = x;
+        next=NULL;
+    }
+};
+void printlist(Node* head)
+{
+ Node* curr = head;
+ while(curr != NULL)
+ {
+    cout<<curr->data<<" ";
+    curr = curr->next;
+ }
+}
+int main(){
+Node* head = new Node(10);
+head->next = new Node(15);
+head->next->next = new Node(30);
+
+printlist(head);
+return 0;
+}
+
